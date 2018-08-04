848c341c9edd6dc555365a1902f9c1c1445296b7
albanie <albanie@robots.ox.ac.uk>
2018-08-04 16:55:43 +0100

    first commit

---
+++ setup_mcnCrossModalEmotions.m
@@ -0,0 +1,10 @@
+function setup_mcnCrossModalEmotions()
+%SETUP_MCNCROSSMODALEMOTIONS Sets up mcnCrossModalEmotions, by adding
+% its folders to the Matlab path
+%
+% Copyright (C) 2018 Samuel Albanie, Arsha Nagrani
+% Licensed under The MIT License [see LICENSE.md for details]
+
+  root = fileparts(mfilename('fullpath')) ;
+  addpath(root, [root '/core'], [root '/scripts']) ;
+  addpath(root, [root '/exps'], [root '/misc']) ;
